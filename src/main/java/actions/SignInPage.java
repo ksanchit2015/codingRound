2507463a8e2d719c644a7edd23f55944a8ea9823
ksanchit2015 <ksanchit2015@gmail.com>
2019-11-24 23:06:36 +0530

    Creation of Page Element & test class basic structure

--- src/main/java/actions/SignInPage.java
+++ src/main/java/actions/SignInPage.java
@@ -1,15 +1,30 @@
 package actions;
 
+import org.openqa.selenium.support.PageFactory;
+import org.openqa.selenium.support.pagefactory.AjaxElementLocatorFactory;
+
 import base.DriverUtilility;
+import bsh.org.objectweb.asm.Type;
 import pages.locators.SignInLocators;
+import utilities.CommonUtilities;
 
 public class SignInPage extends DriverUtilility{
 	
 	
-	public SignInLocators signin;
+	public SignInLocators signInLocator;
+	public CommonUtilities commonUtil;
 
 	public SignInPage(){
+		this.signInLocator = new SignInLocators();
+		AjaxElementLocatorFactory factory = new AjaxElementLocatorFactory(driver,10);
+		PageFactory.initElements(driver, this.signInLocator);
 		
 	}
-
+	
+	public void doLogin(String username, String password){
+		commonUtil.webElementClick(signInLocator.yourTrips);
+		commonUtil.webElementTypeValue(signInLocator.email, username);
+		commonUtil.webElementTypeValue(signInLocator.password, password);
+		commonUtil.webElementClick(signInLocator.submitButton);
+	}
 }
---
+++ src/test/java/testcases/SignInPageTest.java
@@ -0,0 +1,24 @@
+package testcases;
+
+import org.testng.annotations.AfterTest;
+import org.testng.annotations.BeforeTest;
+import org.testng.annotations.Test;
+
+public class SignInPageTest {
+	
+	@BeforeTest
+	public void initializeTest(){
+		
+	}
+	
+	@Test
+	public void signInTest(){
+		
+	}
+	
+	@AfterTest
+	public void tearDown(){
+		
+	}
+
+}
